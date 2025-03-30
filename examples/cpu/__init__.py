723bc2887208bfa8e732562c3f9333d39f99b750
bert82503 <bert825_work@163.com>
2025-03-30 20:36:13 +0800

    Add __init__.py in examples, datasets, cpu

---
+++ examples/cpu/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+from examples.cpu.leke import (
+    LeKe,
+)
+
+__all__ = [
+    "LeKe",
+]
---
+++ examples/datasets/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+homo_datasets = [
+]
+
+hetero_datasets = [
+    'LeKe',
+]
+hyper_datasets = [
+]
+synthetic_datasets = [
+]
+
+__all__ = homo_datasets + hetero_datasets + hyper_datasets + synthetic_datasets

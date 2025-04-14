2f386ee86f0850d63e3696894aafa126a1c3a9f5
bert82503 <bert825_work@163.com>
2025-04-14 19:31:17 +0800

    Improve leke_cpu_deepseek.py

--- examples/cpu/leke_cpu_deepseek.py
+++ examples/cpu/leke_cpu_deepseek.py
@@ -13,124 +13,10 @@ from torch_geometric.sampler.base import NegativeSamplingMode
 from torch_geometric.sampler import NegativeSampling
 
 import torch_geometric.transforms as T
-# from torch_geometric.datasets import Taobao
+from examples.cpu.leke import LeKe
 from torch_geometric.loader import LinkNeighborLoader
 from torch_geometric.nn import SAGEConv
 from torch_geometric.utils.convert import to_scipy_sparse_matrix
-
-import os
-from typing import Callable, Optional
-
-import numpy as np
-
-from torch_geometric.data import (
-    HeteroData,
-    InMemoryDataset,
-    download_url,
-    extract_zip,
-)
-
-class LeKe(InMemoryDataset):
-    r"""Taobao is a dataset of user behaviors from Taobao offered by Alibaba,
-    provided by the `Tianchi Alicloud platform
-    <https://tianchi.aliyun.com/dataset/649>`_.
-
-    Taobao is a heterogeneous graph for recommendation.
-    Nodes represent users with user IDs, items with item IDs, and categories
-    with category ID.
-    Edges between users and items represent different types of user behaviors
-    towards items (alongside with timestamps).
-    Edges between items and categories assign each item to its set of
-    categories.
-
-    Args:
-        root (str): Root directory where the dataset should be saved.
-        transform (callable, optional): A function/transform that takes in an
-            :obj:`torch_geometric.data.HeteroData` object and returns a
-            transformed version. The data object will be transformed before
-            every access. (default: :obj:`None`)
-        pre_transform (callable, optional): A function/transform that takes in
-            an :obj:`torch_geometric.data.HeteroData` object and returns a
-            transformed version. The data object will be transformed before
-            being saved to disk. (default: :obj:`None`)
-        force_reload (bool, optional): Whether to re-process the dataset.
-            (default: :obj:`False`)
-
-    """
-    url = ('https://www.leoao.com/'
-           'LeKeUserBehavior.csv.zip')
-
-    def __init__(
-        self,
-        root: str,
-        transform: Optional[Callable] = None,
-        pre_transform: Optional[Callable] = None,
-        force_reload: bool = False,
-    ) -> None:
-        super().__init__(root, transform, pre_transform,
-                         force_reload=force_reload)
-        self.load(self.processed_paths[0], data_cls=HeteroData)
-
-    @property
-    def raw_file_names(self) -> str:
-        return 'LeKeUserBehavior.csv'
-
-    @property
-    def processed_file_names(self) -> str:
-        return 'data.pt'
-
-    def download(self) -> None:
-        path = download_url(self.url, self.raw_dir)
-        extract_zip(path, self.raw_dir)
-        os.remove(path)
-
-    def process(self) -> None:
-        import pandas as pd
-
-        # user_id,store_id,city_id,behavior_type,behavior_time
-        cols = ['userId', 'storeId', 'cityId', 'behaviorType', 'behaviorTime']
-        df = pd.read_csv(self.raw_paths[0], names=cols)
-
-        # Time representation (YYYY.MM.DD-HH:MM:SS -> Integer)
-        # start: 1511539200 = 2017.11.25-00:00:00
-        # end:   1512316799 = 2017.12.03-23:59:59
-        # TODO
-        # start = 1511539200
-        # end = 1512316799
-        # df = df[(df["behaviorTime"] >= start) & (df["behaviorTime"] <= end)]
-
-        df = df.drop_duplicates()
-
-        # behavior_dict = {'run': 1, 'door': 2, 'gc': 3, 'cp': 4, 'pr': 5}
-        # df['behaviorType'] = df['behaviorType'].map(behavior_dict)
-
-        num_entries = {}
-        for name in ['userId', 'storeId', 'cityId']:
-            # Map IDs to consecutive integers:
-            value, df[name] = np.unique(df[[name]].values, return_inverse=True)
-            num_entries[name] = value.shape[0]
-
-        data = HeteroData()
-
-        data['user'].num_nodes = num_entries['userId']
-        data['store'].num_nodes = num_entries['storeId']
-        data['city'].num_nodes = num_entries['cityId']
-
-        row = torch.from_numpy(df['userId'].values)
-        col = torch.from_numpy(df['storeId'].values)
-        data['user', 'store'].edge_index = torch.stack([row, col], dim=0)
-        data['user', 'store'].time = torch.from_numpy(df['behaviorTime'].values)
-        behavior = torch.from_numpy(df['behaviorType'].values)
-        data['user', 'store'].behavior = behavior
-
-        df = df[['storeId', 'cityId']].drop_duplicates()
-        row = torch.from_numpy(df['storeId'].values)
-        col = torch.from_numpy(df['cityId'].values)
-        data['store', 'city'].edge_index = torch.stack([row, col], dim=0)
-
-        data = data if self.pre_transform is None else self.pre_transform(data)
-
-        self.save([data], self.processed_paths[0])
 
 
 class ItemGNNEncoder(torch.nn.Module):
@@ -305,6 +191,8 @@ def run_train(data, train_data, val_data, test_data, args):
         hidden_channels=64,
         out_channels=64,
     )
+    # 输出网络结构
+    print(model)
     # Initialize lazy modules
     for batch in train_loader:
         _ = model(
@@ -323,28 +211,25 @@ def run_train(data, train_data, val_data, test_data, args):
         print("Val")
         val_auc = test(val_loader)
         best_val_auc = max(best_val_auc, val_auc)
-        print(
-            f'Epoch: {epoch:02d}, Loss: {loss:4f}, Val AUC: {val_auc:.4f}')
+        print(f'Epoch: {epoch:02d}, Loss: {loss:4f}, Val AUC: {val_auc:.4f}')
     print("Test")
     test_auc = test(test_loader)
     print(f'Total {args.epochs:02d} epochs: Final Loss: {loss:4f}, '
           f'Best Val AUC: {best_val_auc:.4f}, '
           f'Test AUC: {test_auc:.4f}')
 
+root_path = osp.join(osp.dirname(osp.realpath(__file__)),
+                         '../../data/LeKe')
+print(root_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_workers', type=int, default=4,
                         help="Number of workers per dataloader")
     parser.add_argument('--lr', type=float, default=0.001)
-    parser.add_argument('--epochs', type=int, default=101)
+    parser.add_argument('--epochs', type=int, default=21)
     parser.add_argument('--batch_size', type=int, default=128)
-    # print(osp.join(osp.dirname(osp.realpath(__file__)),
-    #                      '../../data/LeKe'))
-    parser.add_argument(
-        '--dataset_root_dir', type=str,
-        default=osp.join(osp.dirname(osp.realpath(__file__)),
-                         '../../data/LeKe'))
+    parser.add_argument('--dataset_root_dir', type=str, default=root_path)
     args = parser.parse_args()
 
     def pre_transform(data):
@@ -362,16 +247,18 @@ if __name__ == '__main__':
         return data
 
     dataset = LeKe(args.dataset_root_dir, pre_transform=pre_transform)
+    print(dataset)
     data = dataset[0]
+    print(data)
 
     data['user'].x = torch.arange(0, data['user'].num_nodes)
     data['store'].x = torch.arange(0, data['store'].num_nodes)
 
     # Only consider user<>store relationships for simplicity:
-    # del data['city']
-    # del data['store', 'city']
-    # del data['user', 'store'].time
-    # del data['user', 'store'].behavior
+    del data['city']
+    del data['store', 'city'].edge_index
+    del data['user', 'store'].time
+    del data['user', 'store'].behavior
 
     # Add a reverse ('store', 'rev_to', 'user') relation for message passing:
     data = T.ToUndirected()(data)
